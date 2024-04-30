d659be60b046f5cecfd1e74ddda9703b09d63f76
“manuelzanaboni” <mzana97@gmail.com>
2024-04-30 21:48:02 +0200

    Added relays

--- src/components/client/test-gpio.tsx
+++ src/components/client/test-gpio.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { Children, useState, useTransition } from "react";
 
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Stack } from "@mantine/core";
 
-import { gpio_read_action, gpio_write_action } from "@/service/actions";
-import { GPIO_STATE } from "@/service/globals";
+import { gpio_write_action } from "@/service/actions";
+import { GPIO_STATE, RELAYS } from "@/service/globals";
 
 export default function TestGpio() {
     const [output, setOutput] = useState("");
     const [isPending, startTransition] = useTransition();
 
     // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations#non-form-elements
-    const handleRead = (gpio: number) =>
-        startTransition(async () => {
-            const data = await gpio_read_action(gpio);
-            setOutput(data);
-        });
+    // const handleRead = (gpio: number) =>
+    //     startTransition(async () => {
+    //         const data = await gpio_read_action(gpio);
+    //         setOutput(data);
+    //     });
 
     const handleWrite = (gpio: number, state: GPIO_STATE) =>
         startTransition(async () => {
@@ -24,19 +24,28 @@ export default function TestGpio() {
             setOutput(data);
         });
 
+
     return (
         <>
-            <Group>
-                <Button loading={isPending} onClick={() => handleRead(2)}>
-                    Read
-                </Button>
-                <Button loading={isPending} onClick={() => handleWrite(2, GPIO_STATE.LOW)}>
-                    Write LOW
-                </Button>
-                <Button loading={isPending} onClick={() => handleWrite(2, GPIO_STATE.HIGH)}>
-                    Write HIGH
-                </Button>
-            </Group>
+            {/* <Button loading={isPending} onClick={() => handleRead(2)}>
+                Read
+            </Button> */}
+
+            <Stack gap="md">
+                {Children.toArray(
+                    Object.values(RELAYS).map(relay =>
+                        <Group>
+                            <Button loading={isPending} onClick={() => handleWrite(relay, GPIO_STATE.LOW)}>
+                                {relay}: Write LOW
+                            </Button>
+                            <Button loading={isPending} onClick={() => handleWrite(relay, GPIO_STATE.HIGH)}>
+                                {relay}: Write HIGH
+                            </Button>
+                        </Group>
+                    )
+                )}
+            </Stack>
+
             {`Output: ${output}`}
         </>
     );
--- src/service/globals.ts
+++ src/service/globals.ts
@@ -7,3 +7,11 @@ export enum GPIO_STATE {
     LOW = "0",
     HIGH = "1",
 }
+
+
+export const RELAYS = {
+    RELAY1: 2,
+    RELAY2: 5,
+    RELAY3: 7,
+    RELAY4: 8,
+}
