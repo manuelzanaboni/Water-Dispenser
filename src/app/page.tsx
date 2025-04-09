cad173f2c5260e47f603e89032640b986df3db87
“manuelzanaboni” <mzana97@gmail.com>
2025-04-10 00:35:15 +0200

    Dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use server";
 
+import { connection } from "next/server";
+
 import Dispenser from "@/components/client/Dispenser";
 import { getLastFilterCapacity } from "@/service/db";
 
 // https://github.com/pacocoursey/next-themes
 export default async function Home() {
+    // dynamic rendering
+    await connection();
     // server side data retrieval from DB
     const filterCapacity: number = await getLastFilterCapacity();
 
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,12 +1,15 @@
 "use server";
 
-import { getDispenses, getRefrigeratorHistory } from "@/service/db";
+import { connection } from "next/server";
 
 import DispenseTable from "@/components/client/DispenseTable";
 import RefrigeratorTable from "@/components/client/RefrigeratorTable";
 import SidePageHeader from "@/components/client/SidePageHeader";
+import { getDispenses, getRefrigeratorHistory } from "@/service/db";
 
 export default async function Stats() {
+    // dynamic rendering
+    await connection();
     // server side data retrieval from DB
     const dispenses = await getDispenses();
     const refrigeratorHistory = await getRefrigeratorHistory();
